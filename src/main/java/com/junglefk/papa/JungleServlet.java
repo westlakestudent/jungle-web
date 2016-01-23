47bcdce46f4a331fffee0b66f00488d90cc22407
westlakestudent <westlakestudent@163.com>
2016-01-24 00:06:32 +0800

    first commit

---
+++ src/main/java/com/junglefk/papa/JungleServlet.java
@@ -0,0 +1,22 @@
+package com.junglefk.papa;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JungleServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("papa", "this is papa, who is that?");
+		req.getRequestDispatcher("/jungle.jsp").forward(req, resp);
+	}
+}
